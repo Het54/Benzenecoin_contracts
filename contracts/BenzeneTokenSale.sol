a7125c3b9d23de3e71674f2d061688b53636ae89
maxslimb <kpatel3032@gmail.com>
2022-04-29 20:23:47 +0530

    resolved errors

--- contracts/BenzeneTokenSale.sol
+++ contracts/BenzeneTokenSale.sol
@@ -1,22 +1,22 @@
-pragma solidity >= 0.4.2
+pragma solidity >= 0.4.2;
 
 import "./BenzeneToken.sol";
 
 contract BenzeneTokenSale{
     address payable admin;
-    BenzeneToken public tokenContract
-    unit256 public tokenPrice;
-    uint256 public tokenSold;
+    BenzeneToken public tokenContract;
+    uint256 public tokenPrice;
+    uint256 public tokensSold;
 
     event Sell(address _buyer, uint256 _amount);
 
-    constructor(BenzeneToken _tokenContract, unit256 _tokenPrice) public {
-        admin = msg.sender;
+    constructor(BenzeneToken _tokenContract, uint256 _tokenPrice) public {
+        admin = payable(msg.sender);
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
 
-    function multiply(unit x, uint y) internal pure returns (uint z) {
+    function multiply(uint x, uint y) internal pure returns (uint z) {
         require(y == 0 || (z = x * y) / y == x);
     }
 
@@ -27,7 +27,7 @@ contract BenzeneTokenSale{
 
         require(tokenContract.transfer(msg.sender, _numberOfTokens));
 
-        tokenSold += _numberOfTokens;
+        tokensSold += _numberOfTokens;
 
         emit Sell(msg.sender, _numberOfTokens);
     }
