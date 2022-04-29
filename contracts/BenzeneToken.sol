dadc4a1097f750486aec422c15c1bc3fd8c283c7
maxslimb <kpatel3032@gmail.com>
2022-04-29 19:00:10 +0530

    benzenetoken_transfer fucn

--- contracts/BenzeneToken.sol
+++ contracts/BenzeneToken.sol
@@ -13,6 +13,26 @@ contract BenzeneToken{
         uint256 _value
     );
 
+    event Approval(
+        address indexed _owner,
+        address indexed _spender,
+        uint256 _value
+    );
+
+    mapping (address => uint256) public balanceOf;
+    mapping (address => mapping(address => uint256)) public allowance;
+
+    constructor(uint256 _initialSupply) public{
+        balanceOf[msg.sender] = _initialSupply;
+        totalSupply = _initialSupply;
+    }
+
+    function transfer(address _to, uint256 _value) public returns(bool success) {
+        allowance[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+        return true;
+    }
+
 
 
 
